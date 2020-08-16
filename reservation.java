39d8c2411fe1192d46e9cf764a102c47b04d8113
syamimimuzafar <48729466+syamimimuzafar@users.noreply.github.com>
2020-08-16 18:04:58 +0800

    Create reservation.java

---
+++ reservation.java
@@ -0,0 +1,51 @@
+package javabeans;
+
+public class Reservation {
+
+	private String name;
+	private String noHp;
+	private String email;
+	private String date;
+	private String time;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNoHp() {
+		return noHp;
+	}
+
+	public void setNoHp(String noHp) {
+		this.noHp = noHp;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+}
